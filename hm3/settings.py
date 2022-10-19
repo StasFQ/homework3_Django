f6e2092dbfa6ddc6dd52178db5f178c894ef8cd5
stas <stas37951@gmail.com>
2022-10-19 19:07:05 +0300

    django changes

--- hm3/settings.py
+++ hm3/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-import datetime as dt
+import datetime as dd
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
