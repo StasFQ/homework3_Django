4aae7bbbe7ed5b3addd2de34fadd2efbe375ad57
stas <stas37951@gmail.com>
2022-10-29 09:35:33 +0300

    homework done

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=25)
+    offer = models.ManyToManyField(Product)
+    actions = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Executor(models.Model):
+    city = models.OneToOneField(City, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
--- hm3/settings.py
+++ hm3/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog'
+    'django_extensions',
+    'catalog',
 ]
 
 MIDDLEWARE = [
